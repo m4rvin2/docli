567b613f43362ff0c08b0950f9216283824bc624
Marcelo Franco Filipe Alves <marcelo.franco.alves@gmail.com>
2019-05-02 00:21:46 -0300

    fix(docli): fix import of args

--- docli.go
+++ docli.go
@@ -1,6 +1,6 @@
 package docli
 
-import "github.com/celicoo/docli-old/pkg/args"
+import "github.com/celicoo/docli/pkg/args"
 
 // Args returns the AST of the command-line arguments.
 func Args() args.Args {
