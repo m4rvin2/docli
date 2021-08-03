388b4497bedee15e5789ac7dbcb2cba776df21fd
celicoo <marcelo.franco.alves@gmail.com>
2021-08-03 18:34:20 -0300

    chore(doc): - delete Version constant - update package documentation

--- doc.go
+++ doc.go
@@ -1,8 +1,5 @@
 // Package docli is declarative language for describing command-line interfaces
-// in Go programs. It cuts down the boilerplate to the very minimum, so you can
-// move on to the reason why you're making the app in the first place.
+// in Go programs. It cuts down the boilerplate to the very minimum, so you're
+// free to move on to the reason why you're making the app in the first place.
 // Docli is 100% inspired by docopt.
 package docli
-
-// Version is the docli version.
-var Version = "2.1.0"
