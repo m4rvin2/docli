9ff31c3f99f75bfab9c3230b99ead0b74736edf5
celicoo <marcelo.franco.alves@gmail.com>
2021-09-01 10:05:07 -0300

    docs: update command interface

--- pkg/args/command.go
+++ pkg/args/command.go
@@ -1,15 +1,13 @@
 package args
 
 type command interface {
-	// Doc returns the doc string of the [command].
+	// Doc returns the docstring of a command.
 	Doc() string
-
-	// Run is called if the bind succeeds.
+	// Run is called when the fields of a command are bound with matching
+	// command-line arguments.
 	Run()
-
-	// Help is called when a user calls help [command].
+	// Help is called when a user runs "<command> help".
 	Help()
-
-	// Error is called when a user uses the [command] wrongly.
+	// Error is called when unregistered command-line arguments are passed.
 	Error(error)
 }
