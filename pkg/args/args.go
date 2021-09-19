0e8929d78d0b8209b47b55cff62093f6d4881a0c
celicoo <marcelo.franco.alves@gmail.com>
2021-09-19 20:46:43 -0300

    refactor(args): replace args function with local variable in Parse function

--- pkg/args/args.go
+++ pkg/args/args.go
@@ -7,20 +7,18 @@ import (
 	"github.com/celicoo/docli/v2/internal/reger"
 )
 
-// args returns the command-line arguments, starting after the program name.
-func args() string {
-	// Concatenate the command-line arguments using the U+001F character as
-	// separator.
-	return strings.Join(os.Args[1:], "\u0009")
-}
-
 // Parse returns the AST of the command-line arguments, starting after the
 // program name.
 func Parse() Args {
+	// Concatenate the command-line arguments using the U+001F character as
+	// separator.
+	s := strings.Join(os.Args[1:], "\u0009")
+	// Ignore any errors that might happen when parsing the command-line
+	// arguments. Although this might seem odd, it's preferable over the bad
+	// error handling of Participle.
+	// INFO(celicoo): errors will be nicely handled once I'm done developing
+	// a replacement for Participle.
 	var a Args
-	// We ignore any errors that might happen when parsing the command-line
-	// arguments. Although this looks odd, it's preferable over the bad error
-	// handling of Participle.
-	reger.Build(&a).ParseString(args(), &a)
+	reger.Build(&a).ParseString(s, &a)
 	return a
 }
