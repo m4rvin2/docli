2b93d2d8d4b0af081366d681bc2e49a5ef53dbf9
celicoo <marcelo.franco.alves@gmail.com>
2021-09-19 20:57:51 -0300

    refactor(args): update variable name of InvalidArgumentError receiver

--- pkg/args/errors.go
+++ pkg/args/errors.go
@@ -6,6 +6,6 @@ type InvalidArgumentError struct {
 	Argument string
 }
 
-func (i *InvalidArgumentError) Error() string {
-	return fmt.Sprintf("'%s' is not a valid argument", i.Argument)
+func (e *InvalidArgumentError) Error() string {
+	return fmt.Sprintf("'%s' is not a valid argument", e.Argument)
 }
